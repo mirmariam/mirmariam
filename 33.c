c88d95470d7416e695484d55fdf64c4509a16740
mariam <136911996+mirmariam@users.noreply.github.com>
2024-07-18 21:02:12 +0530

    Update 33.c

--- 33.c
+++ 33.c
@@ -8,11 +8,11 @@
 #include <stdio.h>
 int main() {
 printf("Pattern: \n");
-for (i = 1; i <= 4; i++) {
-   for (space = 3; space >= i; space --) {
+for (int i = 1; i <= 4; i++) {
+   for (int space = 3; space >= i; space --) {
       printf(" ");
    }
-   for (j = 1; j <= i; j++) {
+   for (int j = 1; j <= i; j++) {
       printf("*");
    }
    printf("\n");
