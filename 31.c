59bd5315cd1447df6e0291d592ad806b94ce5a59
mariam <136911996+mirmariam@users.noreply.github.com>
2024-07-18 21:00:35 +0530

    Update 31.c

--- 31.c
+++ 31.c
@@ -7,10 +7,11 @@
 // ******
 
 #include <stdio.h>
+
 int main() {
 printf("Pattern: \n");
-for (i = 1; i <= 5; i++) {
-   for (j = 1; j <= i + 1; j++) {
+for (int i = 1; i <= 5; i++) {
+   for (int j = 1; j <= i + 1; j++) {
     printf("*");
    }
    printf("\n");
