4920532221a9fc143b3b95fad7b083f0eaa4ff9d
mariam <136911996+mirmariam@users.noreply.github.com>
2024-09-10 07:10:15 +0530

    Update 73.c

--- 73.c
+++ 73.c
@@ -3,8 +3,14 @@
 #include<stdio.h>
 
 int main() {
+    char *filename = "file.txt";
     FILE *F = fopen("file.txt", "w");
     char name[20] = "Mariam";
+
+    if(F == NULL) {
+        printf("Error; could not open file %s\n", filename);
+        return 1;
+    }
 
     //this displays name in file
     fprintf(F, "%s\n", name);
